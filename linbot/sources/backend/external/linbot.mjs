92edd56e14b04135a082b1ae7d7265a8b5615fdb
Nadav Tasher <tashernadav@gmail.com>
2021-05-16 23:52:27 +0300

    Updates

--- linbot/sources/backend/external/linbot.mjs
+++ linbot/sources/backend/external/linbot.mjs
@@ -1,66 +1,72 @@
 // Import utilities
 import { Database, File, Hash, Utilities } from "../internal/utilities.mjs";
 import { Client, MessageAttachment } from "discord.js";
+
+import FileSystem from "fs";
 import Process from "child_process";
+import Request from "request";
 
 // Initialize database
 const database = new Database("sessions", {
-	channel: {
-		id: "string",
-		name: "string"
-	},
-	container: {
-		name: "string",
-		type: "string",
-		context: "string"
-	}
+	name: "string",
+	type: "string",
+	context: "string"
 });
 
 // CLI and JS API
 export class Linbot {
 
-	static initialize(channelName, channelID, repository = "ubuntu:20.04") {
+	static initialize(session, repository = "ubuntu:20.04") {
+		// Terminate currently running container
+		Linbot.terminate(session);
+
 		// Create container name
 		let container = Utilities.random(10);
 
-		// Create a new entry in the database
-		database.set(channelID, {
-			channel: {
-				id: channelID,
-				name: channelName
-			},
-			container: {
-				name: container,
-				type: repository,
-				context: "/home/"
-			}
-		});
-
 		// Create the container
-		return Process.execSync(`docker run --pids-limit 50 --tty --detach --name ${container} ${repository} /bin/sh`).toString();
+		Process.execSync(`docker run --pids-limit 50 --tty --detach --name ${container} ${repository} /bin/sh`).toString();
+
+		// Create a new entry in the database
+		database.set(session, {
+			name: container,
+			type: repository,
+			context: "/home/"
+		});
 	}
 
-	static context(sessionID, directory) {
+	static terminate(session) {
+		// Make sure database has entry
+		if (!database.has(session))
+			return;
+
+		// Query entry
+		let entry = database.get(session);
+
+		// Kill container
+		Process.execSync(`docker rm -f ${entry.name}`).toString();
+	}
+
+	static context(session, directory) {
 		// Fetch data from database
-		let entry = database.get(sessionID);
+		let entry = database.get(session);
 
 		// Modify data
-		entry.container.context = directory;
+		entry.context = directory;
 
 		// Update database
-		database.set(sessionID, entry);
+		database.set(session, entry);
 	}
 
-	static execute(sessionID, command) {
+	static execute(session, command) {
 		// Read the database
-		let entry = database.get(sessionID);
+		let entry = database.get(session);
 
 		// Create the output buffer
 		let output;
 
 		// Execute in child process
 		try {
-			output = Process.execSync(`docker exec -w ${Linbot.#escape(entry.container.context)} ${entry.container.name} sh -c ${Linbot.#escape(command)}`).toString();
+			output = Process.execSync(`docker exec -w ${Linbot.#escape(entry.context)} ${entry.name} sh -c ${Linbot.#escape(command)}`).toString();
 		} catch (e) {
 			output = e.message;
 		} finally {
@@ -68,14 +74,14 @@ export class Linbot {
 		}
 	}
 
-	static upload(sessionID, buffer, file) {
+	static upload(session, buffer, file) {
 		// Push the file into the container
-		Linbot.execute(sessionID, `base64 -d '${buffer.to("base64")}' > ${file}`);
+		Linbot.execute(session, `echo ${buffer.toString("base64")} | base64 -d - > ${file}`);
 	}
 
-	static download(sessionID, file) {
+	static download(session, file) {
 		// Pull the file from the container
-		return Buffer.from(Linbot.execute(sessionID, `cat ${file} | base64`), "base64");
+		return Buffer.from(Linbot.execute(session, `cat ${file} | base64`), "base64");
 	}
 
 	static #escape(parameter) {
@@ -91,7 +97,7 @@ client.on("message", (receivedMessage) => {
 	if (receivedMessage.author !== client.user) {
 		// Parse sessionID
 		const channel = receivedMessage.channel;
-		const sessionID = channel.id;
+		const session = channel.id;
 
 		// Try all
 		try {
@@ -104,47 +110,58 @@ client.on("message", (receivedMessage) => {
 				channel.send(`Running command \`${command}\``).then(
 					(message) => {
 						// Execute command
-						let result = Linbot.execute(sessionID, command);
+						let result = Linbot.execute(session, command);
 
 						// Update message
-						message.edit(`\`\`\`bash\n${command}\n\`\`\`\n\`\`\`\n${result.length > 0 ? result : "No output"}\`\`\``);
+						message.edit(`\`\`\`bash\n${command}\n\`\`\`\`\`\`\n${result.length > 0 ? result : "No output"}\`\`\``);
 					}
 				).catch();
 			} else if (receivedMessage.content.startsWith("#")) {
 				// Substitute string
 				let splits = receivedMessage.content.substring(1).trim().split(" ", 2);
+
+				// Parse splits
 				const action = splits.shift();
 				const parameter = splits.shift();
 
 				// Check possible actions
-				if (action === "directory") {
+				if (action === "help") {
+					// Print help message
+					channel.send("```\n#help - show this message\n#restart [IMAGE]- restart your shell\n#upload [PATH] - upload a file\n#download [PATH] - download a path\n#directory [PATH] - change working directory\n```");
+				} else if (action === "directory") {
 					// Change working directory
-					Linbot.context(sessionID, parameter);
-
+					Linbot.context(session, parameter);
 					// Send reply
-					channel.send(`Changed working directory to \`${parameter}\``);
-				} else if (action === "create") {
+					channel.send(`Changed context to \`${parameter}\``);
+				} else if (action === "restart") {
 					// Initialize container
-					Linbot.initialize(channel.name, sessionID, parameter);
-
+					Linbot.initialize(session, parameter);
 					// Send reply
-					channel.send(`Container was created`);
+					channel.send(`Shell was created!`);
 				} else if (action === "upload") {
 					// Check attachement
-					if (!message.attachments) {
-						channel.send("Missing attachment to upload");
+					let attachment;
+					if (!(attachment = receivedMessage.attachments.array().shift())) {
+						channel.send("Missing attachment!");
 						return;
 					}
-
-					// Upload files
-					let attachment = message.attachments.array()[0];
-					// Linbot.upload(sessionID, , parameter);
+					// Download file and push to container
+					Request({
+						url: attachment.url,
+						encoding: null
+					}, (error, response, buffer) => {
+						// Push to container
+						Linbot.upload(session, buffer, parameter);
+						// List files
+						channel.send(`\`\`\`bash\n${Linbot.execute(session, `ls -lahS ${parameter}`)}\`\`\``);
+					});
 				} else if (action === "download") {
-					channel.send(new MessageAttachment(Linbot.download(sessionID, parameter), parameter.split("/").pop()));
+					// Download the file
+					channel.send(new MessageAttachment(Linbot.download(session, parameter), parameter.split("/").pop()));
 				}
 			}
 		} catch (e) {
-			channel.send(`Oops. Error: \`${e.message}\``);
+			channel.send(`Ouch. \`${e.message}\``);
 		}
 	}
 });
@@ -155,14 +172,11 @@ client.login(process.env.DISCORD_TOKEN);
 // Web API routes
 export const Routes = {
 	linbot: {
-		list: {
-
-		},
-		query: {
-
-		},
-		initialize: {
-
+		link: {
+			handler: () => {
+				return process.env.DISCORD_ID;
+			},
+			parameters: {}
 		}
 	}
 };
